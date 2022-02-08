2700ec0427c5ddcf33a894910a42df7e2558cce5
Angad <angad.tendulkar@gmail.com>
2022-02-08 20:22:45 +0000

    ts+nextjs

--- src/utils/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from '../main/App';
-import * as React from 'react';
-
-test('renders learn react link', () => {
-	render(<App />);
-	const linkElement = screen.getByText(/learn react/i);
-	expect(linkElement).toBeInTheDocument();
-});
