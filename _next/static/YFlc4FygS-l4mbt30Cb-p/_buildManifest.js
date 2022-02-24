8199513706d76cf365250ebbc62a0ee5e5641c96
TheAlphaReturns <angad.tendulkar@gmail.com>
2022-02-24 21:43:56 +0000

    Deploying to gh-pages from @ inertia-unblocker/inertia-client@e84288c6863033de6cd6e5c0c24d30a68209e28d 🚀

---
+++ _next/static/YFlc4FygS-l4mbt30Cb-p/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-3e9453b64b30f60a.js"],"/_error":["static/chunks/pages/_error-8022dacb1937fc7a.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- src/utils/hooks/theme.tsx
+++
@@ -1,9 +0,0 @@
-import * as nextUI from '@nextui-org/react';
-import * as nextThemes from 'next-themes';
-
-export function ProxyHook(): [boolean, React.Dispatch<React.SetStateAction<string>>]  {
-	const { setTheme } = nextThemes.useTheme();
-	const { isDark } = nextUI.useTheme();
-
-	return [isDark, setTheme];
-}
