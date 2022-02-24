7124b1ba7a0f151a47758d133bb0836e4bb56de5
Angad Tendulkar <angad.tendulkar@gmail.com>
2022-02-24 16:49:34 -0500

    add assetPrefix, basePath

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true
+	basePath: '/inertia-client',
+	assetPrefix: '/inertia-client',
 };
 
 module.exports = nextConfig;
