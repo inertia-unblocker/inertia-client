5fb38cb8dda3ef2b587530abbb629fe7ef3bcf39
Angad Tendulkar <angad.tendulkar@gmail.com>
2022-03-23 11:13:30 -0400

    config for github pages.

---
+++ .gh-pages-setup.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const fileName = './src/config/baseConfig.json';
+const file = require(fileName);
+    
+file.prefix = "/inertia-client";
+    
+fs.writeFile(fileName, JSON.stringify(file), function writeJSON(err) {
+	if (err) return console.log(err);
+	console.log(JSON.stringify(file, null, 4));
+	console.log('writing to ' + fileName);
+});
